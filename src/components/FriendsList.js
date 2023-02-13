1c83d6b058c1b21203fe7283f1c8f3ce63e8aed8
mdatif796 <mdatif796@gmail.com>
2023-02-13 09:59:58 +0530

    Added friends section on home page

---
+++ src/components/FriendsList.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks';
+import styles from '../styles/home.module.css';
+
+const FriendsList = () => {
+  const auth = useAuth();
+  const { friendships = [] } = auth.user;
+
+  return (
+    <div className={styles.friendsList}>
+      <div className={styles.header}>Friends</div>
+
+      {friendships && friendships.length === 0 && (
+        <div className={styles.noFriends}>NO friends found!</div>
+      )}
+
+      {friendships &&
+        friendships.map((friend) => (
+          <div key={`friend-${friend._id}`}>
+            <Link className={styles.friendsItem} to={`/user/${friend._id}`}>
+              <div className={styles.friendsImg}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png"
+                  alt=""
+                />
+              </div>
+              <div className={styles.friendsName}>{friend.to_user.name}</div>
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default FriendsList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,15 @@ import styles from '../styles/home.module.css';
 import Comment from '../components/Comment';
 import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
-import { Loader } from '../components';
+import { FriendsList, Loader } from '../components';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const auth = useAuth();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -30,56 +33,60 @@ const Home = () => {
   }
 
   return (
-    <div className={styles.postsList}>
-      {posts.map((post) => (
-        <div className={styles.postWrapper} key={`post-${post._id}`}>
-          <div className={styles.postHeader}>
-            <div className={styles.postAvatar}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png"
-                alt="user-pic"
-              />
-              <div>
-                <Link
-                  to={`/user/${post.user._id}`}
-                  className={styles.postAuthor}
-                >
-                  {post.user.name}
-                </Link>
-                <span className={styles.postTime}>a minute ago</span>
-              </div>
-            </div>
-            <div className={styles.postContent}>{post.content}</div>
-
-            <div className={styles.postActions}>
-              <div className={styles.postLike}>
+    <div className={styles.home}>
+      <div className={styles.postsList}>
+        {posts.map((post) => (
+          <div className={styles.postWrapper} key={`post-${post._id}`}>
+            <div className={styles.postHeader}>
+              <div className={styles.postAvatar}>
                 <img
-                  src="https://cdn-icons-png.flaticon.com/512/2589/2589175.png"
-                  alt="likes-icon"
+                  src="https://cdn-icons-png.flaticon.com/512/4140/4140048.png"
+                  alt="user-pic"
                 />
-                <span>{post.likes.length}</span>
+                <div>
+                  <Link
+                    to={`/user/${post.user._id}`}
+                    className={styles.postAuthor}
+                  >
+                    {post.user.name}
+                  </Link>
+                  <span className={styles.postTime}>a minute ago</span>
+                </div>
+              </div>
+              <div className={styles.postContent}>{post.content}</div>
+
+              <div className={styles.postActions}>
+                <div className={styles.postLike}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/2589/2589175.png"
+                    alt="likes-icon"
+                  />
+                  <span>{post.likes.length}</span>
+                </div>
+
+                <div className={styles.postCommentsIcon}>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/3114/3114810.png"
+                    alt="comments-icon"
+                  />
+                  <span>{post.comments.length}</span>
+                </div>
+              </div>
+              <div className={styles.postCommentBox}>
+                <input placeholder="Start typing a comment" />
               </div>
 
-              <div className={styles.postCommentsIcon}>
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/3114/3114810.png"
-                  alt="comments-icon"
-                />
-                <span>{post.comments.length}</span>
-              </div>
+              {post.comments.map((comment) => {
+                return (
+                  <Comment key={`comment-${comment._id}`} comment={comment} />
+                );
+              })}
             </div>
-            <div className={styles.postCommentBox}>
-              <input placeholder="Start typing a comment" />
-            </div>
-
-            {post.comments.map((comment) => {
-              return (
-                <Comment key={`comment-${comment._id}`} comment={comment} />
-              );
-            })}
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      {auth.user && <FriendsList />}
     </div>
   );
 };
